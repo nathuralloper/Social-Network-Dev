f699afbe6127cfe8dcfd533ff1074badb383d99c
Gregory Sanchez <gregory.sanchez@bionuclear.com.do>
2019-09-25 09:34:28 -0400

    jwt was implemented

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,12 @@
 
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
+
 
 const User = require('../../models/User');
 
@@ -56,15 +59,25 @@ router.post('/', [
         await user.save();
 
         //return jsonwebtoken
-        res.send('User registered')
+        const payload = {
+            user: {
+                id: user.id,
+            }
+        };
 
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                console.log(token);
+                if (err) throw err;
+                res.json({ token });
+            });
     } catch (error) {
         console.error(error.message);
         res.status(500).send('Server error');
     }
-
-
-
 });
 
 module.exports = router;
