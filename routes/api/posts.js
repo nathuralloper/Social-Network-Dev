b3308002aaa47fca030f031111a5bb24d32762d6
Gregory Sanchez <gregory.sanchez@bionuclear.com.do>
2019-09-27 13:15:12 -0400

    I like the actions of i like, i dont like and the comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,11 +9,6 @@ const Post = require('../../models/Posts');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
-
-
-
-
-
 // @route   POST api/posts
 // @desc    Create a post
 // @access  Private
@@ -27,7 +22,7 @@ router.post('/', [auth, [
 
     try {
 
-        const user = await User.findById(re.user.id).select('-password');
+        const user = await User.findById(req.user.id).select('-password');
 
         const newPost = new Post({
             text: req.body.text,
@@ -105,6 +100,121 @@ router.delete('/:id', auth, async (req, res) => {
     }
 });
 
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //Check if the post has already been liked
+        if (post.likes.filter(like => like.user.toString() == req.user.id).length > 0) {
+            return res.status(400).json({ msg: 'Post already liked' });
+        }
+
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+
+        res.json(post.likes);
+
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+});
+
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+router.put('/unlike/:id', auth, (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (post.likes.filter(like => like.user.toString() == req.user.id).length === 0) {
+            return res.status(400).json({ msg: 'Post has not yet been liked' });
+        }
+
+        // Get remove index
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+        post.likes.splice(removeIndex, 1);
+        await post.save();
+
+        res.json(post.likes);
+
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+});
+
+// @route   POST api/posts/comment/:id
+// @desc    Comment on a post
+// @access  Private
+router.post('/comment/:id', [auth, [
+    check('text', 'Text is requerid').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await User.findById(req.params.id);
+
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+        await newPost.save();
+
+        res.json(post.comments);
+
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+});
+
+
+// @route   DELETE api/posts/comment/:id/:coment_id
+// @desc    Delete comment
+// @access  Private
+router.delete('/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Pull out comment
+        const comment = await post.comments.find(comment => comment.id === req.params.comment_id);
+
+        //Make sure comment exists
+        if (!comment) {
+            return res.status(404).json({ msg: 'Comment does not exist' });
+        }
+
+        //Check user
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(404).json({ msg: 'User not authorized' });
+        }
+
+        // Get remove index
+        const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+        post.comments.splice(removeIndex, 1);
+        await post.save();
+
+        res.json(post.comments);
+
+    } catch (err) {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server error');
+    }
+});
 
 
 module.exports = router;
