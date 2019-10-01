57b9004fc1da2a3fba42b957b88bafa3d26a053f
Gregory Sanchez <gregory.sanchez@bionuclear.com.do>
2019-10-01 18:10:35 -0400

    the module to authenticate the user is finished

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,23 +1,57 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../../actions/auth";
+import PropTypes from "prop-types";
 
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <a onClick={logout} href="!#">
+          <i className="fas fa-sign-out-alt"></i>{" "}
+          <span className="hide-sm">logout</span>
+        </a>
+      </li>
+    </ul>
+  );
 
-import React from 'react';
-import { Link } from 'react-router-dom';
+  const guestLinks = (
+    <ul>
+      <li>
+        <a href="!#">Developers</a>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </ul>
+  );
 
-const Navbar = () => {
-    return (
-        <nav className="navbar bg-dark">
-            <h1>
-                <Link to="/">
-                    <i className="fas fa-code"></i> DevNetwork
-                </Link>
-            </h1>
-            <ul>
-                <li><a href="!#">Developers</a></li>
-                <li><Link to="/register">Register</Link></li>
-                <li><Link to="/login">Login</Link></li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/">
+          <i className="fas fa-code"></i> DevNetwork
+        </Link>
+      </h1>
+      {<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>}
+    </nav>
+  );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Navbar);
